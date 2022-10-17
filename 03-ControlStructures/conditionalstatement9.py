9f3d5de9e222b0903d98fbd2e316c598b50c9919
Andrei Hlushchanka <114917859+AndreiHlushchanka@users.noreply.github.com>
2022-10-17 12:15:05 +0200

    changes committed

---
+++ 03-ControlStructures/conditionalstatement9.py
@@ -0,0 +1,6 @@
+x = int (input('Number = '))
+y = int (input('Second Number = '))
+if x>0 or y>0:
+    print('One of them is positive')
+elif x<0 and y<0:
+    print('There are no positive numbers')
