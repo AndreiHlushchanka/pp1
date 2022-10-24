f44969a2bfc2882a42a3ce5069594c7c38ebbdba
Andrei Hlushchanka <114917859+AndreiHlushchanka@users.noreply.github.com>
2022-10-24 12:00:48 +0200

    Add files via upload

--- 04-Subroutines/functions5.py
+++ 04-Subroutines/functions5.py
@@ -1,4 +1,5 @@
 def display_university_address():
-    for number in range(2):
      print("Cracow University of Economics\nRakowicka 27\n31-510 Kraków, POLAND")
-display_university_address()
+
+for i in range(2):
+ display_university_address()
