5381a9a477f0d48d200d5fcea780918df29d10d5
Andrei Hlushchanka <114917859+AndreiHlushchanka@users.noreply.github.com>
2022-11-28 12:20:42 +0100

    Add files via upload

---
+++ 08-DictionariesStacksAndQueues/10.py
@@ -0,0 +1,29 @@
+countries = [{
+    "country": "Poland",
+    "population": "37,38 mil",
+   },
+{
+    "country": "Netherlands",
+    "population": "17,53 mil",
+},
+{
+    "country": "Czech Repunlic",
+    "population": "10,7 mil",
+},
+{
+    "country": "Czech Repunlic",
+    "population": "10,7 mil",
+},
+{
+    "country": "Lithuania",
+    "population": "2,795 mil",
+},
+{
+    "country": "Spain",
+    "population": "47,33 mil",
+}]
+
+i=0
+while i<len(countries):
+    print(countries[i])
+    i+=1
